0d537a33bbe14fdc2185c3e52b677974c93aa647
chrishub188 <1971225@stud.hs-mannheim.de>
2020-05-08 22:17:59 +0200

    Added receiver for mqtt messages to map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import * as L from 'leaflet';
 import { LatLngBoundsExpression } from 'leaflet';
+import { Subscription } from 'rxjs';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
 
 
 @Component({
@@ -8,18 +10,28 @@ import { LatLngBoundsExpression } from 'leaflet';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit, AfterViewInit {
+export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
+  private subscription: Subscription;
+  topicname: any;
+  msg: any;
+  isConnected = false;
   private map;
   private tiles;
   private marker;
   private containerMarker;
 
-  constructor() { }
+  constructor(private _mqttService: MqttService) { }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   ngAfterViewInit(): void {
     this.initMap();
   }
 
   ngOnInit(): void {
+    this.subscribeNewTopic('beacon/output');
   }
 
   private initMap(): void {
@@ -34,11 +46,11 @@ export class MapComponent implements OnInit, AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
     /// Add container marker with text popup
-    this.containerMarker = L.icon({
-      iconUrl: 'assets/img/baseline_location_on_black_18dp.png',
-      iconSize: [12, 12], // size of the icon
-      // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    this.containerMarker = L.divIcon({
+      className: 'custom-div-icon',
+      html: "<div style='background-color:#c30b82;' class='marker-pin'></div><i class='material-icons'>gps_fixed</i>",
+      iconSize: [30, 42],
+      iconAnchor: [15, 42]
     });
     this.marker = L.marker([56.465130, -2.926664], { icon: this.containerMarker });
     this.marker.bindPopup('I am a container');
@@ -47,7 +59,21 @@ export class MapComponent implements OnInit, AfterViewInit {
     const imageUrl = 'assets/img/rig_plan_01.svg';
     const imageBounds: LatLngBoundsExpression = [[56.465552, -2.927335], [56.464787, -2.925404]];
     const image = L.imageOverlay(imageUrl, imageBounds).addTo(this.map);
+    // Set view on image
     this.map.setView([56.465130, -2.926664], 25);
   }
 
+  subscribeNewTopic(topic: string): void {
+    this.topicname = topic;
+    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+      this.msg = message;
+      console.log(message.payload.toString().split(','));
+      let messageItems = message.payload.toString().split(',');
+      this.updateMarker(messageItems[3], messageItems[5]);
+    });
+  }
+
+  updateMarker(lat, lng): void {
+    this.marker.setLatLng([lat, lng]).update();
+  }
 }
