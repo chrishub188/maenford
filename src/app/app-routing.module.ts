a42a69780bf17da5fbd9af6583322cab147857b7
chrishub188 <1971225@stud.hs-mannheim.de>
2020-05-15 20:08:45 +0200

    added routes list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { RequestsComponent } from './requests/requests.component';
+import { BackloadComponent } from './backload/backload.component';
+import { LoadoutComponent } from './loadout/loadout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'requests', component: RequestsComponent },
+  { path: 'backload', component: BackloadComponent },
+  { path: 'loadout', component: LoadoutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
