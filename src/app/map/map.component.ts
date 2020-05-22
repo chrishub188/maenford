d6c33e8b265f54b4e7073d609898ff796e447b32
chrishub188 <1971225@stud.hs-mannheim.de>
2020-05-22 23:34:02 +0200

    Increased max zoom on map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -54,7 +54,7 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
     }).addTo(this.map);
     // Add  open streetmap map layer to the map
     this.tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 20,
+      maxZoom: 25,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
     //Add easy button to center location
